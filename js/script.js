a4df6cee9c18f4391fa44f8ac8aaf2cc8a1a6f89
David Israel <davidisrael194@gmail.com>
2019-04-11 11:50:34 +0100

    refactored

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ function getSearchMethod(searchTerm) {
 //this function fetches the api from openweathermap.org
 function searchWeather(searchTerm) {
     getSearchMethod(searchTerm);
-    fetch(`http://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?${searchMethod}=${searchTerm}&APPID=${appId}&units=${units}`)
         .then(result => result.json())
         .then(result => init(result))
 }
